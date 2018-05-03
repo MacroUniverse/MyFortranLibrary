251beb0b16225181e207d35112eff6b5a2e672b2
MacroUniverse <270174408@qq.com>
2018-05-03 14:07:00 -0500

    added a nested loop to increase calculation
    when the array is too large, the "segmentation falt" might occur

--- openMPtest/test.f90
+++ openMPtest/test.f90
@@ -1,16 +1,18 @@
 !Program to test openMP with gfortran
 program openMP
-  integer, parameter :: n = 2E5
-  integer:: i,j
+  integer, parameter :: n = 8E4
+  integer:: i,j,k
   real(kind=8), dimension(n)::  A, B
   do i=1,n
     A(i) = i
   enddo
 !$OMP PARALLEL DO
+  do k=1,n
   do j=1,n
     do i=2,n
       B(i) = B(i) + (A(i) + A(i-1)) / 2.0
     enddo
+  enddo
   enddo
 !$OMP END PARALLEL DO
 end program openMP
